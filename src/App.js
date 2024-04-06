950546d2e05fe3457f343a615780c1c45b8bc77d
Aleksandra Malinovskaya <amilinovskaa@gmail.com>
2024-04-06 23:33:37 +0300

    add context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import './App.css';
 import Context from './Context';
-import Switch from './Switch';
 import Provider from './Provider';
-import {useContext} from 'react';
+import {useState} from 'react';
 
 function App() {
-  const {state} = useContext(Context);
+  const [state, setState] = useState(true);
+
+  const toggleTheme = () => {
+    setState(!state);
+  };
+  
 
   return (
-    <Provider>
-      <Switch />
-      <div
-      style={{
-        background: state === true ? 'black' : 'white',
-        color: state === false ? 'black' : 'white'
-      }}
-    >
-      <h1>zzzzzzz</h1>
-    </div>
-    </Provider>
+    <Context.Provider value = {{state, toggleTheme}}>
+      <Provider />
+    </Context.Provider>
   );
 }
 
--- src/Provider.js
+++ src/Provider.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useContext} from 'react';
 import Context from './Context';
+import moon from './moon.jpg';
+import sun from './sun.jpg';
+import Switch from './Switch';
+import './App.css';
 
-const Provider = ({ child }) => {
-  const [state, setState] = useState(true);
-
-  const toggleTheme = () => {
-    setState(!state);
-  };
+const Provider = () => {
+  const {state} = useContext(Context);
 
   return (
-    <Context.Provider value={{state, toggleTheme}}>
-      {child}
-    </Context.Provider>
+    <div 
+      style={{
+        background: state === true ? 'black' : 'white',
+        color: state === false ? 'black' : 'white'
+      }}
+    >
+      <div className='subject'>
+      <img src = {state === true ? moon : sun} alt='img'/>
+      <p>Toggle Dark or Light customize your interface</p>
+      <Switch />
+      </div>
+    </div>
   );
 };
 
